b537a9983a9f0237cbc3e52e94b7d7f9654d1db8
Jose Candido <jozecandido@macncheese.local>
2023-01-25 17:26:26 -0300

    Adding union flag

--- src/Payroll.Tests/Domain/EmployeeTests.cs
+++ src/Payroll.Tests/Domain/EmployeeTests.cs
@@ -24,4 +24,26 @@ public class EmployeeTests
         Assert.Equal(paymentType, employee.PaymentType);
         Assert.Equal(paymentValue, employee.PaymentValue);
     }
+
+    [Fact]
+    public void New_Employee_Unionized()
+    {
+        // Arrange
+        var name = "Employee";
+        var address = "St Emplyee";
+        var paymentType = PaymentType.Hourly;
+        decimal paymentValue = 10;
+        var isUnionized = true;
+
+        // Act
+        var employee = new Employee(name, address, paymentType, paymentValue, isUnionized);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, employee.Id);
+        Assert.Equal(name, employee.Name);
+        Assert.Equal(address, employee.Address);
+        Assert.Equal(paymentType, employee.PaymentType);
+        Assert.Equal(paymentValue, employee.PaymentValue);
+        Assert.Equal(isUnionized, employee.IsUnionized);
+    }
 }
