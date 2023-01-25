19827eec520793e26bc4a299a6b37442fe86f6d3
Jose Candido <jozecandido@macncheese.local>
2023-01-25 14:07:59 -0300

    Adding Id to TimeCard

--- src/Payroll.Domain/TimeCard.cs
+++ src/Payroll.Domain/TimeCard.cs
@@ -2,12 +2,15 @@ namespace Payroll.Domain;
 
 public class TimeCard
 {
+    public Guid Id { get; private set; }
+
     public Guid EmployeeId { get; private set; }
     public DateOnly Date { get; private set; }
     public decimal HoursWorked { get; private set; }
 
     public TimeCard(Guid employeeId, DateOnly date, decimal hoursWorked)
     {
+        this.Id = Guid.NewGuid();
         this.EmployeeId = employeeId;
         this.Date = date;
         this.HoursWorked = hoursWorked;
