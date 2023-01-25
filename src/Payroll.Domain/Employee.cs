c19408fee17d226a31cea248870b0556d2064be0
Jose Candido <jozecandido@macncheese.local>
2023-01-25 17:39:24 -0300

    Fixing tests

--- src/Payroll.Domain/Employee.cs
+++ src/Payroll.Domain/Employee.cs
@@ -7,15 +7,15 @@ public class Employee
     public string Address { get; private set; }
     public PaymentType PaymentType { get; private set; }
     public decimal PaymentValue { get; private set; }
-    public bool IsUnionized { get; private set; }
+    public decimal Rate { get; set; }
 
-    public Employee(string name, string address, PaymentType paymentType, decimal paymentValue, bool isUnionized = false)
+    public Employee(string name, string address, PaymentType paymentType, decimal paymentValue, decimal rate = 0m)
     {
         this.Id = Guid.NewGuid();
         this.Name = name;
         this.Address = address;
         this.PaymentType = paymentType;
         this.PaymentValue = paymentValue;
-        this.IsUnionized = isUnionized;
+        this.Rate = rate;
     }
 }
--- src/Payroll.Tests/Domain/EmployeeTests.cs
+++ src/Payroll.Tests/Domain/EmployeeTests.cs
@@ -6,13 +6,14 @@ namespace Payroll.Tests.Domain;
 public class EmployeeTests
 {
     [Fact]
-    public void New_Employee()
+    public void New_Employee_Hourly()
     {
         // Arrange
         var name = "Employee";
         var address = "St Emplyee";
         var paymentType = PaymentType.Hourly;
         decimal paymentValue = 10;
+        decimal rate = 0;
 
         // Act
         var employee = new Employee(name, address, paymentType, paymentValue);
@@ -23,21 +24,21 @@ public class EmployeeTests
         Assert.Equal(address, employee.Address);
         Assert.Equal(paymentType, employee.PaymentType);
         Assert.Equal(paymentValue, employee.PaymentValue);
-        Assert.False(employee.IsUnionized);
+        Assert.Equal(rate, employee.Rate);
     }
 
     [Fact]
-    public void New_Employee_Unionized()
+    public void New_Employee_Monthly()
     {
         // Arrange
         var name = "Employee";
         var address = "St Emplyee";
-        var paymentType = PaymentType.Hourly;
-        decimal paymentValue = 10;
-        var isUnionized = true;
+        var paymentType = PaymentType.Monthly;
+        decimal paymentValue = 1000;
+        decimal rate = 0;
 
         // Act
-        var employee = new Employee(name, address, paymentType, paymentValue, isUnionized);
+        var employee = new Employee(name, address, paymentType, paymentValue);
 
         // Assert
         Assert.NotEqual(Guid.Empty, employee.Id);
@@ -45,6 +46,28 @@ public class EmployeeTests
         Assert.Equal(address, employee.Address);
         Assert.Equal(paymentType, employee.PaymentType);
         Assert.Equal(paymentValue, employee.PaymentValue);
-        Assert.True(employee.IsUnionized);
+        Assert.Equal(rate, employee.Rate);
+    }
+
+    [Fact]
+    public void New_Employee_Commisioned()
+    {
+        // Arrange
+        var name = "Employee";
+        var address = "St Emplyee";
+        var paymentType = PaymentType.Commissioned;
+        decimal paymentValue = 1000;
+        decimal rate = 10;
+
+        // Act
+        var employee = new Employee(name, address, paymentType, paymentValue, rate);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, employee.Id);
+        Assert.Equal(name, employee.Name);
+        Assert.Equal(address, employee.Address);
+        Assert.Equal(paymentType, employee.PaymentType);
+        Assert.Equal(paymentValue, employee.PaymentValue);
+        Assert.Equal(rate, employee.Rate);
     }
 }
